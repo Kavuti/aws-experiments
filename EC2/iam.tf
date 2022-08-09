571d4cc83686a161f460c1aa849cd8d310a44f9d
Christian Cavuti <christian.cavuti@gmail.com>
2022-08-09 18:00:06 +0200

    Completed EC2 example setup

--- EC2/iam.tf
+++ EC2/iam.tf
@@ -1,28 +1,39 @@
-# resource "aws_iam_policy" "christian_policy" {
-#   name = "christian-policy-080822"
+data "aws_iam_policy_document" "instance_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
 
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action   = ["ec2:Describe*"]
-#         Effect   = "Allow"
-#         Resource = "*"
-#       },
-#     ]
-#   })
-# }
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
 
-# resource "aws_iam_role" "christian_role" {
-#   name                = "christian_role"
-#   assume_role_policy  = data.aws_iam_policy_document.instance_assume_role_policy.json # (not shown)
-#   managed_policy_arns = [aws_iam_policy.christian_policy.arn, ]
+resource "aws_iam_role" "christian_role" {
+  name                = "christian_role"
 
-#   tags = {
-#     service = "ec2"
-#   }
+  assume_role_policy  = data.aws_iam_policy_document.instance_assume_role_policy.json
 
-#   lifecycle {
-#     ignore_changes = [tags, ]
-#   }
-# }
+  tags = {
+    service = "ec2"
+  }
+
+  inline_policy {
+    name = "christian-policy-080822"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   = ["ec2:Describe*"]
+          Effect   = "Allow"
+          Resource = "*"
+        },
+      ]
+    })
+  }
+
+  lifecycle {
+    ignore_changes = [tags, ]
+  }
+}
--- EC2/main.tf
+++ EC2/main.tf
@@ -7,9 +7,16 @@ data "aws_ami" "amazon_linux_ami" {
   }
 }
 
+resource "aws_iam_instance_profile" "christian_instance_profile" {
+  name = "christian_instance_profile"
+  role = aws_iam_role.christian_role.name
+}
+
 resource "aws_instance" "christian_instance" {
   ami           = data.aws_ami.amazon_linux_ami.id
   instance_type = "t2.micro"
+  
+  iam_instance_profile = aws_iam_instance_profile.christian_instance_profile.name
 
   security_groups = [aws_security_group.christian_security_group.name, ]
 
